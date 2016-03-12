a0e46f873c68643a891e0f13d981f81f65dc06ab
star <star@startekiMini.lan>
2016-03-12 22:31:12 +0800

    get vim and liteIDE develop env

---
+++ go/src/golang_program/doc.go
@@ -0,0 +1,6 @@
+// golang_program project doc.go
+
+/*
+golang_program document
+*/
+package main
---
+++ go/src/golang_program/main.go
@@ -0,0 +1,10 @@
+// golang_program project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
